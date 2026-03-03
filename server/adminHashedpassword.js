439e4cbe47c6f3b5e6b8177884dc5fc7b8d2be80
DesuRaghavendra <desu.raghavendra2023@vitstudent.ac.in>
2026-03-03 23:38:00 +0530

    stop tracking admin file

--- server/adminHashedpassword.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcryptjs');
-
-async function generateHashedPassword() {
-    const salt = await bcrypt.genSalt(10);  // generate salt
-    const hashedPassword = await bcrypt.hash('23BCE0322', salt);  // hash your admin password
-    console.log(hashedPassword);  // this will print the hashed password
-}
-
-generateHashedPassword();
